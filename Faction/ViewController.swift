9f506ed2896aa569494999cc08d0484243592ff7
alessandroparisi <alessandro.parisi@mail.mcgill.ca>
2015-01-20 13:50:56 -0500

    Initial Commit

---
+++ Faction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Faction
+//
+//  Created by Alessandro Parisi on 2015-01-20.
+//  Copyright (c) 2015 Alessandro Parisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
