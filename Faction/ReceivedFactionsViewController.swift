b213423df8a4eaa78cc39ba3b76fd92c8e519e8b
Leonardo Siracusa <leonardo.siracusa@mail.mcgill.ca>
2015-02-07 04:27:00 -0500

    Outline for receive faction functionality
    segueing the info doesn’t work, but I don’t think it needs to since
    we’ll be able to access that info from the other class anyway?

--- Faction/ReceivedFactionsViewController.swift
+++ Faction/ReceivedFactionsViewController.swift
@@ -13,7 +13,9 @@ class ReceivedFactionsViewController : UIViewController, UITableViewDelegate, UI
     @IBOutlet var tableView: UITableView!
     
     
-    var factions = [String]()
+    var factions: [String] = ["Does Ale have a bad bitch?", "Sup homes, this true", "I aint going public"]
+    var sender: [String] = ["Bobby Morrisane", "Robert Montoya", "Elon Muskatchi"]
+    var currentText: String = ""
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -46,6 +48,8 @@ class ReceivedFactionsViewController : UIViewController, UITableViewDelegate, UI
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
         
+        // DISPLAY DATA
+        cell.textLabel?.text = self.sender[indexPath.row] as String
         return cell
     }
     
@@ -55,25 +59,38 @@ class ReceivedFactionsViewController : UIViewController, UITableViewDelegate, UI
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Faction at row: \(indexPath.row): has string: \(factions[indexPath.row])")
+        currentText = factions[indexPath.row]
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var vc = segue.destinationViewController as AnswerFactionViewController
+        println("Current text \(currentText)")
+        vc.tableViewText = currentText
+        
+    }
+    
     
     func getFactions() -> Void {
         println("a")
-        let url = NSURL(string: path + "/api/update")
-        let session = NSURLSession.sharedSession()
-        let dataTask = session.dataTaskWithURL(url!, completionHandler: { (data: NSData!, response:NSURLResponse!,
-            error: NSError!) -> Void in
-            println("ok")
-            println(response)
-            println(error)
-            println(data)
-            var err: NSError?
-            let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &err) as NSDictionary
-            println(jsonDict)
-
-            self.factions = jsonDict["factions"] as Array
-            println(self.factions)
-        })
+        
+    
+        
+//        let url = NSURL(string: path + "/api/update")
+//        let session = NSURLSession.sharedSession()
+//        let dataTask = session.dataTaskWithURL(url!, completionHandler: { (data: NSData!, response:NSURLResponse!,
+//            error: NSError!) -> Void in
+//            println("ok")
+//            println(response)
+//            println(error)
+//            println(data)
+//            var err: NSError?
+//            let jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &err) as NSDictionary
+//            println(jsonDict)
+//
+//            self.factions = jsonDict["factions"] as Array
+//            println(self.factions)
+//        })
     }
     
 }
